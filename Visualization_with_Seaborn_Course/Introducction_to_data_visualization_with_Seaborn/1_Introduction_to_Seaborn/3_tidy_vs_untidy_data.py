072a014e68b3e80ae461227d55bffd48f6f700af
Sembofer <fernando.valencia.hdez@gmail.com>
2022-07-13 20:49:30 -0500

    Finish Introduction to Seaborn

---
+++ Visualization_with_Seaborn_Course/Introducction_to_data_visualization_with_Seaborn/1_Introduction_to_Seaborn/3_tidy_vs_untidy_data.py
@@ -0,0 +1,8 @@
+# Import pandas
+import pandas as pd
+
+# Create a DataFrame from csv file
+df = pd.read_csv(csv_filepath)
+
+# Print the head of df
+print(df.head())
---
+++ Visualization_with_Seaborn_Course/Introducction_to_data_visualization_with_Seaborn/3_Visualizing_a_categorical_and_a_quantitative_variable/1_count_plots.py
@@ -0,0 +1,7 @@
+# Separate into column subplots based on age category
+sns.catplot(y="Internet usage", data=survey_data,
+            kind="count",
+            col="Age Category")
+
+# Show plot
+plt.show()
